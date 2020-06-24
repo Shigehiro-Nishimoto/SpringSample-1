ff01b29ebe453f6cdd2610dc772e41e5f500795a
hayato fujimura <h-fujimura@rough_and_ready.co.jp>
2020-06-24 18:13:04 +0900

    ８章で、画面作成中

---
+++ src/main/java/com/example/demo/domain/model/repository/UserDao.java
@@ -0,0 +1,5 @@
+package com.example.demo.domain.model.repository;
+
+public class UserDao {
+
+}
---
+++ src/main/java/com/example/demo/domain/model/repository/jdbc/UserDaoJdbcImpl.java
@@ -0,0 +1,5 @@
+package com.example.demo.domain.model.repository.jdbc;
+
+public class UserDaoJdbcImpl {
+
+}
